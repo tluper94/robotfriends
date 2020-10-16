7a276096cae92d338d7cd7179ec28d3eab1e3857
Trevor Luper <luper4@gmail.com>
2020-10-16 01:28:23 -0500

    Changed App class to functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cardlist from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
+const App = () => {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchField] = useState('');
 
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
-
-	componentDidMount() {
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response=> response.json())
-		.then(users => this.setState({ robots: users}));
-	}
+			.then((response) => response.json())
+			.then((users) => setRobots(users));
+	}, []);
 
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
-	}
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value);
+	};
 
-	render(){
-		const { robots, searchfield} = this.state;
-		const filteredRobots = robots.filter(robots => {
-			return robots.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		return !robots.length ? 
-		<h1>Loading</h1> :
-		(
-			<div className='tc'>
-				<h1 className='f2'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll>
-				<Cardlist robots={filteredRobots} />
-				</Scroll>
-			</div>
-		)
-	}
-}
+	const filteredRobots = robots.filter((robots) => {
+		return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
+
+	return !robots.length ? (
+		<h1>Loading</h1>
+	) : (
+		<div className='tc'>
+			<h1 className='f2'>RoboFriends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			<Cardlist robots={filteredRobots} />
+		</div>
+	);
+};
 export default App;
